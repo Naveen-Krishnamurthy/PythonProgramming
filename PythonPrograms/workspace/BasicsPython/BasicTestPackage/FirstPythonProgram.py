145e74589dc79a597bd43d1b64571e944330862e
Naveen <Naveen3665@hotmail.com>
2017-05-03 23:11:15 +0530

    May 3rd Python Project creation in eclipse

---
+++ PythonPrograms/workspace/BasicsPython/BasicTestPackage/FirstPythonProgram.py
@@ -0,0 +1,11 @@
+'''
+Created on 03-May-2017
+
+@author: ubuser
+'''
+print("Welcome to Python")
+print("What's your name :")
+name=input()
+print("What's your age :")
+age=input()
+print("Your name is :"+name+" and the age is :"+str(age))
