32f656f0b2e11b97d600913e0ad72175f423abd0
Naveen <Naveen3665@hotmail.com>
2017-05-06 00:23:36 +0530

    May 6th 2017 Testing

--- PythonPrograms/workspace/BasicsPython/BasicTestPackage/FirstPythonProgram.py
+++ PythonPrograms/workspace/BasicsPython/BasicTestPackage/FirstPythonProgram.py
@@ -3,9 +3,24 @@ Created on 03-May-2017
 
 @author: ubuser
 '''
+'''
 print("Welcome to Python")
 print("What's your name :")
 name=input()
 print("What's your age :")
 age=input()
 print("Your name is :"+name+" and the age is :"+str(age))
+'''
+spam=input()
+print(spam)
+spam=(int(spam))*10/2
+
+print(spam)
+
+print(42==42.00)
+print(42=='this')
+print(42==True)
+
+
+print(round(35.66))
+print(round(23.22))
